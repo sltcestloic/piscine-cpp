57cde4046e362f0293e9d2dbf6161eb454fd6337
Loïc <lbertran@student.42lyon.fr>
2021-09-14 14:19:42 +0200

    ex00

---
+++ day0/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int ac, char **av)
+{
+	(void)av;
+	if (ac == 1)
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+	else
+	{
+		string args;
+		for (int i = 1; i < ac; i++)
+		{
+			args.append(av[i]);
+			args.append(" ");
+		}
+		args.pop_back();
+		transform(args.begin(), args.end(), args.begin(), ::toupper);
+		cout << args << endl;
+	}
+}
