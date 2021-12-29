f7013ba2e6c4ca7391e9f8e66bcac0e22fcefe8c
Loïc <lbertran@student.42lyon.fr>
2021-12-29 13:54:30 +0100

    ok

--- day01/ex05/Karen.cpp
+++ day01/ex05/Karen.cpp
@@ -20,11 +20,11 @@ void Karen::debug() {
 }
 
 void Karen::info() {
-	cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << endl;
+	cout << "I cannot believe adding extra bacon cost more money. You don't put enough! If you did I would not have to ask for it!" << endl;
 }
 
 void Karen::warning() {
-	cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << endl;
+	cout << "I think I deserve to have some extra bacon for free. I've been coming here for years and you just started working here last month." << endl;
 }
 
 void Karen::error() {
